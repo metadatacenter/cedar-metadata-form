dea31a91a251f9ba47022253e0454ac7dbc29cd8
Debra Willrett <willrett@stanford.edu>
2019-05-31 15:55:58 -0700

    initial version of cedar-form

--- cedar-form/src/app/customelement/customelement/customelement.component.ts
+++ cedar-form/src/app/customelement/customelement/customelement.component.ts
@@ -1,17 +1,51 @@
-import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input,  OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {FormControl, FormGroup} from '@angular/forms';
+
+class Stats {
+  constructor(
+    readonly a: number,
+    readonly b: number,
+    readonly c: number,
+    readonly value: any,
+  ) { }
+}
 
 @Component({
-  selector: 'cedar-form',
+  selector: 'app-custom-element',
   templateUrl: './customelement.component.html',
   styleUrls: ['./customelement.component.less'],
-  encapsulation: ViewEncapsulation.None // <- this allows CSS to bleed to this component from parent app
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CustomelementComponent  {
+export class CustomelementComponent implements OnInit {
 
-  @Input() name: string;
-  @Output() helloEvt: EventEmitter<string> = new EventEmitter();
+  @Input() a: number;
+  @Input() b: number;
+  @Input() c: number;
 
-  constructor() { }
+  name: FormControl;
+  form: FormGroup;
 
+  private statsSubject = new BehaviorSubject<Stats>(null);
+  public stats$ = this.statsSubject.asObservable();
+
+
+  ngOnInit(): void {
+    this.name = new FormControl('start');
+    this.form = new FormGroup({});
+    this.form.addControl('name', this.name);
+
+    this.statsSubject.next(new Stats(this.a, this.b, this.c, this.form.value));
+    console.log('a', this.a, 'b', this.b, 'c',  this.c, this.form.value);
+  }
+
+  more(): void {
+    this.a = Math.round(Math.random() * 100);
+    this.b = Math.round(Math.random() * 100);
+    this.c = Math.round(Math.random() * 100);
+
+    // this.cd.markForCheck();
+    this.statsSubject.next(new Stats(this.a, this.b, this.c, this.form.value));
+  }
 
 }
--- cedar-form/src/main.ts
+++ cedar-form/src/main.ts
@@ -4,9 +4,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare global {
+  interface Window {
+    WebComponents: {
+      ready: boolean;
+    };
+  }
+}
+
+// needed for jsonld js library
+(window as any).global = window;
+
+
+
 if (environment.production) {
   enableProdMode();
 }
+
+
 
 platformBrowserDynamic()
   .bootstrapModule(
