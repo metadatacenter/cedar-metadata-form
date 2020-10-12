3a5acc6a1df4006f0b86743fbd1562fff031d042
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-12 14:45:00 -0700

    1=1=2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,10 +15,8 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it(' 1 + 1 should be equal 2', () => {
+    expect(1 + 1).toEqual(2);
   });
 
 });
